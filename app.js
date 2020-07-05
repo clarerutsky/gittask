a3bbbd3e896b12827e23a58300a8ed4ce20bba31
Rutsky <rutsky1200@gmail.com>
2020-07-05 18:06:24 +0300

    -----

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log(sports);
 //another way to remove item
  delete delete1[5];
 console.log(sports," ");
-// Accessing each element
+//another way to remove item
 delete1= sports.shift();
 console.log(delete1," ");
 
@@ -24,11 +24,17 @@ console.log(content);
 var  content = "this is just demos";
 console.log(content);
 console.log(typeof(content));
+
+//OBject and Deletion
 var student_detail = {
   "NAME": "Mike", "Age": 25, "Major": "Chemistry","code": 2323
 };
  student_detail.mark =" 70%";
 console.log("Student details are:", student_detail);
+//Deleing items in OBJECT
+ delete  student_detail.code;
+ console.log("Student details are:", student_detail);
+
 // Adding item
  var num = [2,1,4,6,7,9.0,20];
  console.log(num," ");
